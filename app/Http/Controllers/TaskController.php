74c26ea68bbd9ae947eb74943272f816c1c138e2
limnimoldev-netizen <git config --global user.email limnimol.dev@gmail.com>
2026-05-05 09:01:45 +0700

    flow ot project done

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
+
 
 class TaskController extends Controller
 {
@@ -15,23 +17,25 @@ class TaskController extends Controller
         return view('tasks.index', compact('tasks'));
     }
 
-    public function create()
-    {
-        return view('tasks.create');
-    }
-
     public function store(Request $request)
     {
-        $request->validate([
-            'title' => 'required',
-            'status' => 'required',
-            'date' => 'required|date',
-            'user_id' => 'required'
+        $title= $request->input('title');
+        $status= $request->input('status');
+        $date= $request->input('date');
+        $user_id= $request->input('user_id');
+        
+            Task::create([
+            'title' => $title,
+            'status' => $status,
+            'date' => $date,
+            'user_id' => $user_id,
         ]);
 
-        Task::create($request->all());
+        
+        return redirect()->route('tasks.index');
 
-        return redirect()->route('tasks.index')->with('success', 'Task created successfully!');
     }
 }
+
+
 
