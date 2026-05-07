d411b0a3c56db035f225108cffd4b61611069dc7
ronoaz96-oss <ronoaz96@gmail.com>
2026-05-07 13:41:07 +0700

    test commit from profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -8,7 +8,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
- 
+ @extends('layouts.app')
+
+@section('content')
    
     <div class="max-w-7xl mx-auto mt-[100px] bg-blue-100 p-6 rounded-lg shadow-md">
          <h1 class="font-bold pb-5 text-2xl">My Profile </h1>
@@ -18,6 +20,6 @@
         <p class="font-bold text-2xl pb-2 pl-6 w-screen">Lucky</p>
         <p class="text-gray-700">Email: johndoe@example.com</p>
     </div>
- 
+ @endsection
 </body>
 </html>
